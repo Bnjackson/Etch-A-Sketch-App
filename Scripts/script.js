42b2a492e8d617ce367efd58b6b314b88e540d72
Bnjackson <65509144+Bnjackson@users.noreply.github.com>
2020-09-22 15:39:43 +0100

    Update script.js

--- Scripts/script.js
+++ Scripts/script.js
@@ -1,34 +1,26 @@
 /*ETCH-A-SKETCH APP
-*The size input determines the number of rows and columns the grid will contain 1 represents 1 row and column 10 represents 10 columns and 10 rows. 
-*The clear button will reset the box container.
+*The size input determines the number of rows and columns by squaring the size.value and setting the rows and columns to the result.
+*The clear button will set the background of the boxes to white.
 *Use CSS grid to create a grid of square boxs.
 *Hovering over a square will change the background color to a randomised color.
 */
 
-//CLEAR
-const clearButton = document.getElementById("clear-button");
-clearButton.addEventListener("click", containerReset);
-
-function containerReset() {
-    document.getElementById("box-container").reset();
-}
 
 //GRID
-
-
-
-
 const container = document.getElementById("box-container");
 const size = document.getElementById("size-input");
 size.addEventListener('input', sizeOfGrid);
+const square = document.createElement("div");
 
 function sizeOfGrid() {
     container.innerHTML = "";
+    container.style.setProperty("--grid-rows", size.value);
+    container.style.setProperty("--grid-rows", size.value);
     numberOfSquares = size.value * size.value; 
     console.log(numberOfSquares);
     for(i = 0; i < numberOfSquares; i++) {
         const square = document.createElement("div");
-        square.className = "sqaure";
+        square.className = "square";
         container.appendChild(square);
         square.addEventListener("mouseover", function (){
             let randomColour = "#" + Math.random().toString(16).substr(-6);
@@ -37,11 +29,16 @@ function sizeOfGrid() {
     }
 }
 
-function colorChange () {
-    let randomColour = "#" + Math.random().toString(16).substr(-6);
-    square.style.background = randomColour;
-}
+//CLEAR
+const clearButton = document.getElementById("clear-button");
+clearButton.addEventListener("click", containerReset);
 
+function containerReset() {
+    squares = document.getElementsByClassName('square');
+    for(i = 0; i < squares.length; i++) {
+        squares[i].style.background = "#ffffff";   
+    }  
+}
 
 
 
